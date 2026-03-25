6a282fc015ef98f252695606a84dcf4d173ae1ef
camila.michel <camila.michel@ab-inbev.com>
2026-03-25 12:36:01 -0300

    Allow CORS - again

--- src/index.js
+++ src/index.js
@@ -18,8 +18,12 @@ import { startCron } from './cron.js'
 const app = express()
 const PORT = process.env.PORT || 3333
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(',')
+  : ['http://localhost:5174']
+
 app.use(cors({
-  origin: 'http://localhost:5174',
+  origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
